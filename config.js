002f389a680201aa84090e95531012c99f7a146f
yusufaa000-oss <yusufaa000@gmail.com>
2026-09-20 15:06:12 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,10 @@
 const SUPABASE_URL = "https://khnbzvgthxbihhatoyov.supabase.co";
 
-const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_ffK5ICyv4m37UjGA6C2zTA_Y799O2oB";
+const SUPABASE_PUBLISHABLE_KEY =
+  "sb_publishable_ffK5ICyv4m37UjGA6C2zTA_Y799O2oB";
+
+window.SUPABASE_URL = SUPABASE_URL;
+window.SUPABASE_PUBLISHABLE_KEY = SUPABASE_PUBLISHABLE_KEY;
 
 const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
