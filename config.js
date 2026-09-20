e4b1dd4aabcfc4f374c7223290fa39aab2fa24cb
yusufaa000-oss <yusufaa000@gmail.com>
2026-09-20 11:31:51 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://khnbzvgthxbihhatoyov.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_ffK5ICyv4m37UjGA6C2zTA_Y799O2oB";
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
+);
